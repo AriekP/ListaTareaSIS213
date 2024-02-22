efb48fb22595bed24748e2f9bd0a5f1cb8692a15
jackelinss <124317565+jackelinss@users.noreply.github.com>
2024-02-22 11:57:44 -0400

    Update archivoOrganizador.java

--- archivoPrincipal.java
+++ archivoPrincipal.java
@@ -1,35 +1,54 @@
-import javax.swing.*;
- 
-/**
- * ESTA CLASE DEBE HEREDAR DE JFRAME PARA PODER
- * CREAR LA VENTANA DE NUESTRA INTERFAZ
- */
-public class PintarVentana extends JFrame{
-     
-    /* Esta variable no es necesaria, pero si
-     * muy recomendable, ya que nos evitar problemas
-     * si hay futuras actualizaciones de la aplicacion*/
-    private static final long serialVersionUID = 1L;
- 
-    public JPanel lamina = new JPanel(); 
-     
- 
-    public PintarVentana() {
-          
-        this.setSize(450, 300);
-         
-        this.setTitle("Colorear ventanas");
-          
-        // Lo centramos en pantalla
-        this.setLocationRelativeTo(null);
-          
-        /* Le decimos que no se puede redimensionar*/
-        this.setResizable(false);
-          
-        // Le decimos que al cerrar la ventana, acabe el programa
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-          
-       
-        this.getContentPane().add(lamina);
-    }
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Organizador extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Organizador frame = new Organizador();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Organizador() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				 setVisible(false);
+	                VentanaOrganizador ventana1 = new VentanaOrganizador();
+	                ventana1.setVisible(true);
+			}
+		});
+		btnNewButton.setBounds(140, 112, 85, 21);
+		contentPane.add(btnNewButton);
+	}
 }
